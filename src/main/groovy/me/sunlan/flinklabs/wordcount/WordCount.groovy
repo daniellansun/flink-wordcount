8104e7134b76fed3b8f54ad6849613adfc58bc6b
Daniel Sun <sunlan@apache.org>
2019-12-09 17:07:50 +0800

    Trivial refactoring

--- src/main/groovy/me/sunlan/flinklabs/wordcount/WordCount.groovy
+++ src/main/groovy/me/sunlan/flinklabs/wordcount/WordCount.groovy
@@ -42,7 +42,7 @@ class WordCount {
         )
         .keyBy(0)
         .timeWindow(Time.seconds(TIME_WINDOW_SECONDS))
-        .reduce((t1, t2) -> new Tuple2<>(t1.f0, t1.f1 + t2.f1))
+        .reduce((t1, t2) -> Tuple2.of(t1.f0, t1.f1 + t2.f1))
         //                .sum(1)
 
         DataStreamSink<Tuple2<String, Integer>> print = windowCounts.print()
